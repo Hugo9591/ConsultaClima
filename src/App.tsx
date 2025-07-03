dd3e4e447a08a004b088a5f389e47af9af3041d9
Hugo Bv <hugo95919@gmail.com>
2025-07-03 02:35:15 -0600

    Clima

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import styles from './App.module.css'
+import Form from './components/Form/Form'
+import useWeather from './hooks/useWeather'
+import WeatherDetail from './components/WeatherDetail/WeatherDetail'
+import Spinner from './components/Spinner/Spinner';
+import Alert from './components/Alert/Alert';
+
+function App() {
+
+  const { weather, fetchWeather, notFound, hasWeatherData, loading } = useWeather();
+
+  return (
+    <>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <Form 
+          fetchWeather={fetchWeather}/>
+
+        {loading && <Spinner/>}
+        
+        {hasWeatherData && <WeatherDetail weather={weather}/>}
+
+        {notFound && <Alert>Ciudad No Encontrada</Alert>}
+        
+      </div>
+    </>
+    
+  )
+}
+
+export default App
